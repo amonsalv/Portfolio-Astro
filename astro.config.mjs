29dab1fa14e83604b915450c59567b5911ab2da4
amonsalv <anamonsalve96@gmail.com>
2024-08-21 20:23:03 -0500

    refactor: Update Astro configuration to use @astrojs/vercel/serverless adapter for server output and enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   // base: '/Portfolio-Astro/', // Descomentar si tu proyecto está en un subdirectorio
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
